1448fdfe53653b0eeb85c5f710eebeba7c394316
Moe-Zart <mohamedzreika03@outlook.com>
2022-07-06 16:54:57 +1000

    initial commit

---
+++ src/components/ToDo.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ToDo = () => {
+    return (
+        <div>
+            <h1>To Do List</h1>
+            <ul>
+                <input type="checkbox" name="" id="" /><p>Study react</p>
+                <input type="checkbox" name="" id="" /><p>Create react project</p>
+                <input type="checkbox" name="" id="" /><p>Create project clone</p>
+                <input type="checkbox" name="" id="" /><p>Fix personal website</p>
+                <input type="checkbox" name="" id="" /><p>Apply for internships + junior roles</p>
+            </ul>
+        </div>
+    );
+}
+
+export default ToDo;
